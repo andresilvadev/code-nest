34d3624dec32d9ca978a0458c9f5ea77f84052d9
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2020-06-28 23:58:04 -0300

    Protejendo rota de criação de usuários administrativos

--- back-end/src/users/users.controller.ts
+++ back-end/src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe, UseGuards } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { ReturnUserDto } from './dto/return-user.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
@@ -9,6 +10,7 @@ export class UsersController {
     constructor(private usersService: UsersService) {}
 
     @Post()
+    @UseGuards(AuthGuard())
     async createAdminUser(@Body(ValidationPipe) createUserDto: CreateUserDto): Promise<ReturnUserDto> {
       const user = await this.usersService.createAdminUser(createUserDto);
       return {
